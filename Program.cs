52809e1ac66f4ee8cf6378755d59ed90cf64d898
Rui Lopes <rgl@ruilopes.com>
2018-02-17 07:14:10 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using Vestris.ResourceLib;
+
+namespace ResourceExtractor
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                ShowHelp();
+                return 1;
+            }
+
+            var filename = args[1];
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("The executable {0} does not exist", filename);
+                return 1;
+            }
+
+            switch (args[0])
+            {
+                case "list":
+                    List(filename);
+                    return 0;
+                case "extract":
+                    if (args.Length < 4)
+                    {
+                        ShowHelp();
+                        return 1;
+                    }
+                    Extract(filename, args[2], args[3]);
+                    return 0;
+                default:
+                    Console.WriteLine("Unknown command");
+                    return 1;
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("ResourceExtractor list <executable>");
+            Console.WriteLine("ResourceExtractor extract <executable> <id> <extract to file name>");
+        }
+
+        private static void Enumerate(string filename, Func<string, Resource, bool> visit)
+        {
+            using (var resources = new ResourceInfo())
+            {
+                resources.Load(filename);
+
+                foreach (var resource in resources)
+                {
+                    var resourceId = string.Format("{0}/{1}/{2}", resource.Type.TypeName, resource.Name, resource.Language);
+
+                    if (visit(resourceId, resource))
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void List(string filename)
+        {
+            Enumerate(filename, (resourceId, resource) =>
+            {
+                Console.WriteLine("{0}\t{1}", resourceId, resource.Size);
+                return false;
+            });
+        }
+
+        private static void Extract(string filename, string id, string destinationFilename)
+        {
+            Enumerate(filename, (resourceId, resource) =>
+            {
+                if (resourceId == id)
+                {
+                    File.WriteAllBytes(destinationFilename, resource.WriteAndGetBytes());
+                    return true;
+                }
+                return false;
+            });
+        }
+   }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+﻿This lists and extract the resources embedded inside an executable file (`.exe`, `.dll`, etc.).
+
+
+To list the resources use, e.g.:
+
+	ResourceExtractor list Procmon.exe
+
+Which will show something like:
+
+	BINRES/RCDRIVERNT/1033  73480
+	BINRES/1308/1033        1186440
+	RT_CURSOR/23/1033       308
+	RT_CURSOR/24/1033       308
+	RT_BITMAP/400/1033      2920
+	RT_ICON/1/1033  3752
+
+The first column is the resource id and the second the resource size.
+
+
+To extract a specific resource into a file use, e.g.:
+
+	ResourceExtractor extract Procmon.exe BINRES/1308/1033 ProcmonAmd64.exe
+
+
+The single executable `ResourceExtractor.exe` was created using [LibZ](https://github.com/MiloszKrajewski/LibZ):
+
+	libz inject-dll --assembly ResourceExtractor.exe --include *.dll --move
+
+
+This uses the [Vestris.ResourceLib C# File Resource Management Library](https://github.com/resourcelib/resourcelib) library.
